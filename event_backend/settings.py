653f6b67b34b8b9c01248e1e5dfb277838f8b932
Afraz <124431362+afrazaffu2@users.noreply.github.com>
2025-07-05 23:34:35 +0530

    Update settings.py

--- event_backend/settings.py
+++ event_backend/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -38,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'event_backend.urls'
 
